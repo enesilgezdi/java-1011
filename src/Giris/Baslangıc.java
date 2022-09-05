61f81dc431f96ed1bd5b200462bb5821f10b56f4
Enes İlgezdi <enesilgezdi@gmail.com>
2022-09-05 14:37:56 +0300

    main

---
+++ src/Giris/Baslangıc.java
@@ -0,0 +1,7 @@
+package Giris;
+
+public class Baslangıc {
+    public static void main(String[] args) {
+        System.out.print("Merhaba Dünya !");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       // Değişkenleri oluştur
+        int mat, fizik,kimya, turkce ,tarih,muzik;
+
+        // Scaner sınıfımızı tanımladık
+        Scanner inp = new Scanner(System.in);
+
+        //Kullanacıdan değerleri al
+        System.out.println("Matematik Notunuz : ");
+        mat = inp.nextInt();
+
+        System.out.println("fizik Notunuz : ");
+        fizik = inp.nextInt();
+
+        System.out.println("Kimya Notunuz : ");
+        kimya = inp.nextInt();
+
+        System.out.println("turkce Notunuz : ");
+        turkce = inp.nextInt();
+
+        System.out.println("Tarih Notunuz : ");
+        tarih = inp.nextInt();
+
+        System.out.println("Muzik Notunuz : ");
+        muzik = inp.nextInt();
+
+        int toplam = (mat + fizik + kimya + turkce + tarih + muzik );
+        double sonuc = toplam / 6;
+        System.out.println(sonuc >= 60 ? "sınıfı gecti" : "Sınıfta Kaldı");
+
+
+
+
+
+
+
+    }
+}

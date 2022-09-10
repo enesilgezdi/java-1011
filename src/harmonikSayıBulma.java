c708ebb6d899a1b0f35296a0ab452392d2daa08b
Enes İlgezdi <enesilgezdi@gmail.com>
2022-09-10 14:47:53 +0300

    hermonik sayı bulma

---
+++ src/harmonikSayıBulma.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class harmonikSayıBulma {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("N sayısını giriniz : ");
+        int n = inp.nextInt();
+        double result = 0.0;
+        for (int i = 1; i<=n ; i ++){
+            result+=(1.0/i);
+        }
+        System.out.println(result);
+    }
+}

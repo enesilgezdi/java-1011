6cc571d27c8939d5f2155550c72f1505266c53ac
Enes İlgezdi <enesilgezdi@gmail.com>
2022-09-15 15:05:58 +0300

    Harmonic Average

---
+++ src/ElamanlarinOrtalaması.java
@@ -0,0 +1,14 @@
+
+public class ElamanlarinOrtalaması {
+    public static void main(String[] args) {
+
+        int [] list ={1, 2 , 3 , 4 , 5};
+
+        double result = 0.0;
+        for (int i = 0 ; i < list.length; i++){
+            result += (1.0/ list[i]);
+        }
+
+        System.out.println("Harmonic Average : " + list.length/ result);
+    }
+}

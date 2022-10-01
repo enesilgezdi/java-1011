28c7996c1d13222b4a89fbc71879fef880fb3ede
Enes İlgezdi <enesilgezdi@gmail.com>
2022-10-01 13:27:23 +0300

    Yeni sayi

--- src/palindromSayi.java
+++ src/palindromSayi.java
@@ -4,14 +4,14 @@ public class palindromSayi {
     static boolean isPalindrom(int number){
         int temp = number, reverseNumber =0 , lastNumber;
         while (temp !=0 ){
-            //System.out.println("==========");
-            //System.out.println("sayı => " + temp);
+            System.out.println("==========");
+            System.out.println("sayı => " + temp);
 
             lastNumber = temp % 10;
-            //System.out.println("son basamak :" +lastNumber);
+            System.out.println("son basamak :" +lastNumber);
 
             reverseNumber = (reverseNumber*10) + lastNumber;
-            //System.out.println("yeni sayı => : " + reverseNumber);
+            System.out.println("yeni sayı => : " + reverseNumber);
             temp /= 10;
         }
         if(number == reverseNumber)
